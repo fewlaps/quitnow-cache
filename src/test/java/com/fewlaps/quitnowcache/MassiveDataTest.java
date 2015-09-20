53b9ed53de2ceae6831f5e59ec7d77628cad4ec3
Roc Boronat <roc@fewlaps.com>
2015-09-20 02:09:51 +0200

    Adding a test to check if the QNCache can work with a milion of entries

---
+++ src/test/java/com/fewlaps/quitnowcache/MassiveDataTest.java
@@ -0,0 +1,26 @@
+package com.fewlaps.quitnowcache;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MassiveDataTest extends BaseTest {
+    QNCache cache;
+
+    @Before
+    public void init() {
+        cache = new QNCacheBuilder().createQNCache();
+    }
+
+    @Test
+    public void worksWithAMilionOfEntries() {
+        int iterations = 1000000;
+        for (int i = 0; i < iterations; i++) {
+            cache.set(Integer.valueOf(i).toString(), i, 0);
+        }
+        for (int i = 0; i < iterations; i++) {
+            assertEquals(i, cache.get(Integer.valueOf(i).toString()));
+        }
+    }
+}
