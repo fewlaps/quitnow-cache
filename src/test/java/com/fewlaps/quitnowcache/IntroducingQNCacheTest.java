3be4dd99590ce8077da69518d770aa25da995ff2
Roc Boronat <roc@fewlaps.com>
2015-09-20 14:23:58 +0200

    Updating the introduction code test

--- src/test/java/com/fewlaps/quitnowcache/IntroducingQNCacheTest.java
+++ src/test/java/com/fewlaps/quitnowcache/IntroducingQNCacheTest.java
@@ -8,44 +8,36 @@ public class IntroducingQNCacheTest {
 
     @Test
     public void theCodeOfTheReadmeWorks() {
-        try {
-            QNCache cache; // Let me introduce you: The simplest memcached-like cache!
+        //##The sample
 
-            //It has a fancy builder with fancy default options.
-            cache = new QNCacheBuilder().createQNCache();
+        QNCache cache = new QNCacheBuilder().createQNCache();
 
-            cache.set("Key", "Value"); //you can save things in the cache,
-            cache.get("Key"); //get it again,
-            cache.remove("Key"); //and remove it if you want.
+        cache.set("key", "value", 60 * 1000); // It can store things for a minute,
+        cache.set("key", "value", 60 * 60 * 1000); // for an hour,
+        cache.set("key", "value", 0); // or forever.
+        cache.set("key", "value"); // And also for the short version of forever.
 
-            //By the way, you can save only it for a second, as you do with memcached.
-            cache.set("Key", "Value", 1000); //save it for a second
-            cache.get("Key"); //it will work as usual,
-            Thread.sleep(1000); //but after a second,
-            cache.get("Key"); //it will become null.
+        cache.get("key"); // It can get them again,
+        cache.remove("key"); // and remove it if you want.
 
-            //In addition, you can save all kinds of Objects:
-            cache.set("Mama", new Integer(42));
-            cache.set("Papa", new ArrayList());
+        cache.get("unExistingKey"); // If something doesn't exists, it returns null
+        cache.get("tooOldKey"); // The same if a key is too old
 
-            //What about the memory? By default, all the instances will be stored unless you remove it.
-            //But there's a way to remove all the instances that are too old: the autorelease parameter.
-            //You can create an autoreleased QNCache using its builder.
-            //For example, cleaning it every minute:
-            cache = new QNCacheBuilder().setAutoReleaseInSeconds(60).createQNCache();
+        cache.set("AnInteger", new Integer(42)); // You can save all kind of Objects...
+        cache.set("ACollection", new ArrayList()); // ...whatever you want
 
-            //And, what about the keys? Are they case sensitive?
-            //By default, yes, as it is the common Java behaviour.
-            //But you can also define it at building time:
-            cache = new QNCacheBuilder().setCaseSensitiveKeys(false).createQNCache();
+        cache.removeAll(); // You can also clean it,
+        cache.size(); // and ask it how many elements it has
 
-            //To end this beautiful writing, let's detail the builder default values:
-            cache = new QNCacheBuilder()
-                    .setAutoReleaseInSeconds(60) //default null: it means autorelease disabled
-                    .setCaseSensitiveKeys(false) //default true
-                    .createQNCache();
-        } catch (InterruptedException ie) {
-            ie.printStackTrace();
-        }
+        //##Let's talk about the memory
+        cache = new QNCacheBuilder().setAutoReleaseInSeconds(1).createQNCache(); //frees the memory every second
+        cache = new QNCacheBuilder().setAutoReleaseInSeconds(60).createQNCache(); //frees the memory every minute
+        cache = new QNCacheBuilder().setAutoReleaseInSeconds(60*60).createQNCache(); //frees the memory every hour
+        cache = new QNCacheBuilder().createQNCache(); //never frees the memory
+
+        //##Are the keys case sensitive?
+        cache = new QNCacheBuilder().setCaseSensitiveKeys(true).createQNCache(); //"key" and "KEY" will be different items
+        cache = new QNCacheBuilder().setCaseSensitiveKeys(false).createQNCache(); //"key" and "KEY" will be the same
+        cache = new QNCacheBuilder().createQNCache(); //"key" and "KEY" will be different items
     }
 }
