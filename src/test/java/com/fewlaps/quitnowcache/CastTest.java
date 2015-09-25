8e94bf44964e499ca481427775e15f0ae2dfd03e
Roc Boronat <roc@fewlaps.com>
2015-09-25 18:52:06 +0200

    Cleaning some tests

--- src/test/java/com/fewlaps/quitnowcache/CastTest.java
+++ src/test/java/com/fewlaps/quitnowcache/CastTest.java
@@ -5,10 +5,8 @@ import com.fewlaps.quitnowcache.beam.ObjectTestTwo;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 import static org.hamcrest.core.IsInstanceOf.instanceOf;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
 public class CastTest extends BaseTest {
@@ -22,65 +20,46 @@ public class CastTest extends BaseTest {
     @Test
     public void saveObjectAndReturnSameInstance() {
         cache.set(A_KEY, new ObjectTestOne());
-        cache.set(ANOTHER_KEY, new ObjectTestTwo());
 
         assertThat(cache.get(A_KEY), instanceOf(ObjectTestOne.class));
-        assertThat(cache.get(ANOTHER_KEY), instanceOf(ObjectTestTwo.class));
     }
 
     @Test
     public void shouldThrowExceptionOnCastReturnObject() {
-        AtomicInteger errors = new AtomicInteger(0);
-
         cache.set(A_KEY, new ObjectTestOne());
-        cache.set(ANOTHER_KEY, new ObjectTestTwo());
+
+        Exception e = null;
 
         try {
-            ObjectTestTwo testTwo = (ObjectTestTwo) cache.get(A_KEY);
+            ObjectTestTwo o = (ObjectTestTwo) cache.get(A_KEY);
         } catch (ClassCastException cle) {
-            errors.set(errors.get() + 1);
+            e = cle;
         }
 
-        try {
-            ObjectTestOne testOne = (ObjectTestOne) cache.get(ANOTHER_KEY);
-        } catch (ClassCastException cle) {
-            errors.set(errors.get() + 1);
-        }
-
-        assertEquals(2, errors.get());
+        assertNotNull(e);
     }
 
     @Test
     public void shouldCastObject() {
         cache.set(A_KEY, new ObjectTestOne());
-        cache.set(ANOTHER_KEY, new ObjectTestTwo());
 
-        ObjectTestOne objectTestOne = cache.<ObjectTestOne>get(A_KEY);
-        ObjectTestTwo objectTestTwo = cache.<ObjectTestTwo>get(ANOTHER_KEY);
+        ObjectTestOne objectTestOne = cache.get(A_KEY);
 
         assertThat(objectTestOne, instanceOf(ObjectTestOne.class));
-        assertThat(objectTestTwo, instanceOf(ObjectTestTwo.class));
     }
 
     @Test
     public void shouldThrowExceptionOnCastWithGenericReturnObject() {
-        AtomicInteger errors = new AtomicInteger(0);
-
         cache.set(A_KEY, new ObjectTestOne());
-        cache.set(ANOTHER_KEY, new ObjectTestTwo());
+
+        Exception e = null;
 
         try {
-            ObjectTestTwo testTwo = cache.get(A_KEY);
+            ObjectTestTwo o = cache.get(A_KEY);
         } catch (ClassCastException cle) {
-            errors.set(errors.get() + 1);
+            e = cle;
         }
 
-        try {
-            ObjectTestOne testOne = cache.get(ANOTHER_KEY);
-        } catch (ClassCastException cle) {
-            errors.set(errors.get() + 1);
-        }
-
-        assertEquals(2, errors.get());
+        assertNotNull(e);
     }
 }
--- src/test/java/com/fewlaps/quitnowcache/MassiveDataTest.java
+++ src/test/java/com/fewlaps/quitnowcache/MassiveDataTest.java
@@ -14,13 +14,12 @@ public class MassiveDataTest extends BaseTest {
     }
 
     @Test
-    public void worksWithAMilionOfEntries() {
-        //int iterations = 1000000; //Travis doesn't like this huge tests :(
-        int iterations = 1000;
+    public void worksWithLotsOfEntries() {
+        int iterations = 10000;
         for (int i = 0; i < iterations; i++) {
             cache.set(Integer.valueOf(i).toString(), i, 0);
         }
-        for (int i = 0; i < iterations; i++) {
+        for (Integer i = 0; i < iterations; i++) {
             assertEquals(i, cache.get(Integer.valueOf(i).toString()));
         }
     }
