ba668008749cc48fd416c04f0af115e538e3f2af
Roc Boronat <roc@fewlaps.com>
2016-06-08 00:47:10 +0200

    Add tests that checks time values using TimeUnit values

--- src/test/java/com/fewlaps/quitnowcache/BuilderTest.java
+++ src/test/java/com/fewlaps/quitnowcache/BuilderTest.java
@@ -2,6 +2,8 @@ package com.fewlaps.quitnowcache;
 
 import org.junit.Test;
 
+import java.util.concurrent.TimeUnit;
+
 import static org.junit.Assert.*;
 
 public class BuilderTest {
@@ -44,10 +46,45 @@ public class BuilderTest {
     }
 
     @Test
+    public void testSetting10AutoReleaseSecondsBuilder_usingSecondsTimeUnit() {
+        QNCache<Object>  cache = new QNCacheBuilder().setAutoRelease(10, TimeUnit.SECONDS).createQNCache();
+
+        assertEquals(Integer.valueOf(10), cache.getAutoReleaseInSeconds());
+    }
+
+    @Test
+    public void testSetting10AutoReleaseSecondsBuilder_usingMillisTimeUnit() {
+        QNCache<Object>  cache = new QNCacheBuilder().setAutoRelease(10000, TimeUnit.MILLISECONDS).createQNCache();
+
+        assertEquals(Integer.valueOf(10), cache.getAutoReleaseInSeconds());
+    }
+
+    @Test
+    public void testSetting10AutoReleaseSecondsBuilder_usingMicrosTimeUnit() {
+        QNCache<Object>  cache = new QNCacheBuilder().setAutoRelease(10000000, TimeUnit.MICROSECONDS).createQNCache();
+
+        assertEquals(Integer.valueOf(10), cache.getAutoReleaseInSeconds());
+    }
+
+    @Test
     public void testSettingDefaultKeepaliveBuilder_with10() {
         QNCache<Object>  cache = new QNCacheBuilder().setDefaultKeepaliveInMillis(10).createQNCache();
 
         assertEquals(Long.valueOf(10), cache.getDefaultKeepaliveInMillis());
+    }
+
+    @Test
+    public void testSettingDefaultKeepaliveBuilder_with10000MillisTimeUnits() {
+        QNCache<Object>  cache = new QNCacheBuilder().setDefaultKeepalive(10000, TimeUnit.MILLISECONDS).createQNCache();
+
+        assertEquals(Long.valueOf(10000), cache.getDefaultKeepaliveInMillis());
+    }
+
+    @Test
+    public void testSettingDefaultKeepaliveBuilder_with10SecondsTimeUnits() {
+        QNCache<Object>  cache = new QNCacheBuilder().setDefaultKeepalive(10, TimeUnit.SECONDS).createQNCache();
+
+        assertEquals(Long.valueOf(10000), cache.getDefaultKeepaliveInMillis());
     }
 
     @Test
@@ -58,8 +95,22 @@ public class BuilderTest {
     }
 
     @Test
+    public void testSettingDefaultKeepaliveBuilder_withZeroSecondsTimeUnit() {
+        QNCache<Object>  cache = new QNCacheBuilder().setDefaultKeepalive(0, TimeUnit.SECONDS).createQNCache();
+
+        assertNull(cache.getDefaultKeepaliveInMillis());
+    }
+
+    @Test
     public void testSettingDefaultKeepaliveBuilder_withMinusTen() {
         QNCache<Object>  cache = new QNCacheBuilder().setDefaultKeepaliveInMillis(-10).createQNCache();
+
+        assertNull(cache.getDefaultKeepaliveInMillis());
+    }
+
+    @Test
+    public void testSettingDefaultKeepaliveBuilder_withMinusTenSecondsTimeUnit() {
+        QNCache<Object>  cache = new QNCacheBuilder().setDefaultKeepalive(-10, TimeUnit.SECONDS).createQNCache();
 
         assertNull(cache.getDefaultKeepaliveInMillis());
     }
